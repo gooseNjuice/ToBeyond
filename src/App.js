68f793fe0d9818506f15abbd17f38485585b2ecc
evgenykononov <mailto:evgeny@unplugged-systems.com>
2022-06-06 16:28:21 +0300

    eslint strict rules (also for tests) sass, webVitals, CSSReset, stylelint and bundle size analyzer added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import './App.scss';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// eslint-disable-next-line react/jsx-filename-extension
+const App = () => <div className="App">
+    <header className="App-header">
+        <img
+            alt="logo"
+            className="App-logo"
+            src={logo}
+        />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Edit
+            {' '}
+
+            <code>
+                src/App.js
+            </code>
+
+            {' '}
+            and save to reload.
         </p>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+            target="_blank"
         >
-          Learn React
+            Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+    </header>
+</div>;
+
 
 export default App;
