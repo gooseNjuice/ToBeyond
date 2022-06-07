183bff41a22c33e1f7c11ded66a4da6981888e0d
evgenykononov <mailto:evgeny@unplugged-systems.com>
2022-06-07 13:31:20 +0300

    index

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
+/* eslint-disable react/jsx-filename-extension */
 import './App.scss';
-import logo from './logo.svg';
+import {useEffect, useRef, useState} from 'react';
+import GLOBE from 'vanta/dist/vanta.globe.min';
+import PrimeReact from 'primereact/api';
 
+PrimeReact.ripple = true;
 // eslint-disable-next-line react/jsx-filename-extension
-const App = () => <div className="App">
-    <header className="App-header">
-        <img
-            alt="logo"
-            className="App-logo"
-            src={logo}
-        />
+const App = () => {
 
-        <p>
-            Edit
-            {' '}
+    const [
+            vantaEffect,
+            setVantaEffect
+        ] = useState(),
+        myRef = useRef(null);
+    useEffect(
+        () => {
 
-            <code>
-                src/App.js
-            </code>
+            if (!vantaEffect) {
 
-            {' '}
-            and save to reload.
-        </p>
+                // eslint-disable-next-line new-cap
+                setVantaEffect(GLOBE({
+                    'el': myRef.current,
+                    'mouseControls': true,
+                    'touchControls': true,
+                    'gyroControls': false,
+                    'minHeight': 200.00,
+                    'minWidth': 200.00,
+                    'scale': 1.00,
+                    'scaleMobile': 1.00,
+                    'color': '#604593',
+                    'color2': '#4b3b6b',
+                    'backgroundColor': '#17151f'
+                }));
 
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            rel="noopener noreferrer"
-            target="_blank"
-        >
-            Learn React
-        </a>
-    </header>
-</div>;
+            }
+            return () => {
+
+                if (vantaEffect) {
+
+                    vantaEffect.destroy();
+
+                }
+
+            };
+
+        },
+        [vantaEffect]
+    );
+    return <header
+        className="hero"
+        id="my-background"
+        ref={myRef}>
+
+        <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+        </h1>
+    </header>;
+
+};
 
 
 export default App;
